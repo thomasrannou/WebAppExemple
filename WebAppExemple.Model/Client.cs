9ae2fa0c546ff4b6e2ed84265f8d41559bd762cf
thomas.rannou <thomas.rannou@orange.com>
2018-05-25 14:45:10 +0200

    Ajout projet

---
+++ WebAppExemple.Model/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAppExemple.Model
+{
+    public class Client
+    {
+        /// <summary>
+        /// Identifiant technique
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nom de famille
+        /// </summary>
+        public string Nom { get; set; }
+
+        /// <summary>
+        /// Prénom
+        /// </summary>
+        public string Prenom { get; set; }
+
+        /// <summary>
+        /// Société
+        /// </summary>
+        public string Societe { get; set; }
+    }
+}
---
+++ WebAppExemple.Model/ContexteExemple.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAppExemple.Model
+{
+    public class ContexteExemple : DbContext
+    {
+        /// <summary>
+        /// Collection de clients
+        /// </summary>
+        public DbSet<Client> Clients { get; set; }
+
+        /// <summary>
+        /// Constructeur avec options
+        /// </summary>
+        /// <param name="options"></param>
+        public ContexteExemple(DbContextOptions<ContexteExemple> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Constructeur de base
+        /// </summary>
+        public ContexteExemple() : base()
+        {
+        }
+    }
+}
